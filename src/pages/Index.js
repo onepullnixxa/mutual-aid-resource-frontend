dbe89157799e51f5e3ea678f8a39356a407eb75a
onepullnixxa <briseno.chris@gmail.com>
2021-10-21 16:27:00 -0400

    returning to previous display

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,31 +1,6 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Index(props) {
-  // State to hold form data
-  const [newForm, setNewForm] = useState({
-    name: "",
-    img: "",
-    donateLink: "",
-    contentText: "",
-  });
-
-  // handleChange function for form
-  const handleChange = (event) => {
-    setNewForm({ ...newForm, [event.target.name]: event.target.value });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    props.createPersons(newForm);
-    setNewForm({
-      name: "",
-      img: "",
-      donateLink: "",
-      contentText: "",
-    });
-  };
-
   const loaded = () => {
     return props.person.map((individual) => (
       <div key={individual._id} className="individual">
@@ -47,42 +22,7 @@ function Index(props) {
     return <h1>Just a moment...</h1>;
   };
 
-  return (
-    <section>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={newForm.name}
-          name="name"
-          placeholder="Individual's Name"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          value={newForm.img}
-          name="image"
-          placeholder="Individual's Image URL"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          value={newForm.donateLink}
-          name="donate-link"
-          placeholder="Donation Link"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          value={newForm.contentText}
-          name="content-text"
-          placeholder="What would you like to say?"
-          onChange={handleChange}
-        />
-        <input type="submit" value="Submit Post" />
-      </form>
-      {props.people ? loaded() : loading()}
-    </section>
-  );
+  return props.person ? loaded() : loading();
 }
 
 export default Index;
