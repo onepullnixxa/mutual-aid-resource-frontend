0b9c26d8db1c44b0eff58cdf47b6d497b8cc94c0
onepullnixxa <briseno.chris@gmail.com>
2021-10-24 18:56:45 -0400

    utilizes shuffle array on index page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 
 function Index(props) {
-  
+
   const loaded = () => {
-    return props.person.map((individual) => (
-      <div key={individual._id} className="individual" style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+    return (props.person.map((individual) => (
+      <div key={individual._id} className="individual">
         <Link to={`/home/${individual._id}`}>
           <img
             src={individual.img}
@@ -12,19 +12,34 @@ function Index(props) {
             className="person-icon"
           />
         </Link>
-        <h3>{individual.name}</h3>
-        <h3>{individual.donateLink}</h3>
-        <p>{individual.contentText}</p>
+        <div className="individual-text">
+          <h3 id="individual-name">{individual.name}</h3>
+          <h3 id="individual-link">{individual.donateLink}</h3>
+          <p id="individual-text">{individual.contentText}</p>
+        </div>
       </div>
-    ));
+    )));
   };
+
+function shuffle(array) {
+  let currentIndex = array.length, randomIndex;
+  
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
 
   const loading = () => {
     return <h1>Just a moment...</h1>;
-    
   };
 
-  return props.person ? loaded() : loading();
+  return props.person ? shuffle(loaded()) : loading();
 }
 
 export default Index;
