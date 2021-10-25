ce186225de60ed6bca70e24b00b50ca10ce30fe6
onepullnixxa <briseno.chris@gmail.com>
2021-10-25 17:41:39 -0400

    adds tribute message

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,6 +3,8 @@ function About () {
         <div className="aboutPageText">
             <h3 className="firstLineText">Mutual Aid Resource is a website intent on boosting aid requests from </h3> <br />
             <h4 className="secondLineText">Black trans, queer, and nonbinary</h4><em className="italicText">individuals</em><h3 className="fourthLineText">in need of aid or assistance.</h3>
+
+            <em className="tributeText">This website is dedicated in memory of Oluwatoyin Salau.</em>
         </div>
     )
 };
